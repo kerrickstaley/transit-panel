53d94484bca5c141ee4d5c827c39e5b9a078a0b0
Kerrick Staley <kerrick@kerrickstaley.com>
2021-10-09 10:35:08 -0400

    Display available bikes at Newport Pkwy station

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 "use strict";
 
-document.querySelector('body').innerHTML = 'Goodbye, world...';
+var newport_pkwy_station_id = '3199';
+var station_status_url = 'https://gbfs.citibikenyc.com/gbfs/en/station_status.json';
+
+var request = new XMLHttpRequest();
+request.open('GET', station_status_url, true);
+request.onload = function() {
+    var data = JSON.parse(request.response);
+    var station_status = data.data.stations.find(el => el.station_id === newport_pkwy_station_id);
+    document.querySelector('body').innerHTML = 'Bikes available: ' + station_status.num_bikes_available;
+};
+request.send();
