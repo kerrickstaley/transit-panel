a68d739bd08ca09acc236ee86e2d9366a9582bf0
Kerrick Staley <kerrick@kerrickstaley.com>
2021-10-17 17:27:58 -0400

    Brookfield ferry time updates

--- main.js
+++ main.js
@@ -47,7 +47,111 @@ function populatePathToWtcInfo(elem) {
         elem.innerHTML = "PATH to WTC";
     };
     request.send();
-}; 
+};
 // populatePathToWtcInfo(document.querySelector(".row"))
 
 
+// https://www.nywaterway.com/HobokenNJTT-WFCRoute.aspx#weekday
+const hobokenToBrookfieldWeekdayDepartureTimes = [
+    '6:05 AM',
+    '6:20 AM',
+    '6:40 AM',
+    '7:00 AM',
+    '7:20 AM',
+    '7:40 AM',
+    '8:00 AM',
+    '8:20 AM',
+    '8:40 AM',
+    '9:00 AM',
+    '9:20 AM',
+    '9:40 AM',
+    '10:00 AM',
+    '10:20 AM',
+    '10:40 AM',
+    '11:00 AM',
+    '11:20 AM',
+    '11:40 AM',
+    '12:00 PM',
+    '12:20 PM',
+    '12:40 PM',
+    '1:00 PM',
+    '1:20 PM',
+    '1:40 PM',
+    '2:00 PM',
+    '2:20 PM',
+    '2:40 PM',
+    '3:00 PM',
+    '3:20 PM',
+    '3:40 PM',
+    '4:00 PM',
+    '4:20 PM',
+    '4:40 PM',
+    '5:00 PM',
+    '5:20 PM',
+    '5:40 PM',
+    '6:00 PM',
+    '6:20 PM',
+    '6:40 PM',
+    '7:00 PM',
+];
+
+const walkTimeFromAptDoorToFerrySec = 10 * 60;
+
+// Take a list of departure times, expressed as strings like "8:30 AM", and get the number of seconds
+// from a given Date to those times. Times are interpreted as the next instance of that time, in the
+// local timezone, that is not before the given Date.
+function getSecUntilDepartures(date, departureTimes) {
+    var DateTime = luxon.DateTime;
+    var Duration = luxon.Duration;
+    const datetime = DateTime.fromJSDate(date);
+    return departureTimes.map(time_ampm => {
+        let [time, ampm] = time_ampm.split(' ');
+        let [hour_str, minute] = time.split(':');
+        let hour = parseInt(hour_str);
+        if (ampm === 'AM' && hour == 12) {
+            hour = 0;
+        }
+        if (ampm === 'PM' && hour != 12) {
+            hour += 12;
+        }
+        var obj = datetime.toObject();
+        obj.hour = hour;
+        obj.minute = minute;
+        obj.second = 0;
+        obj.millisecond = 0;
+        var departure = DateTime.fromObject(obj);
+        if (departure < datetime) {
+            departure += Duration.fromObject({days: 1});
+        }
+        var ret = (departure - datetime) / 1000;
+        return ret;
+    });
+}
+
+// Get number of seconds until next departure. Takes a list of departure strings, expressed as strings
+// "8:30 AM" and a Date.
+//
+// The return value is the number of seconds until the first departure time that is not before `date`.
+function getSecUntilNextDeparture(date, departureTimes) {
+    return Math.min(...getSecUntilDepartures(date, departureTimes));
+}
+
+function getFerryLeaveSec(date, departureTimes, walkTimeSec) {
+    var queryDate = new Date(date + walkTimeSec * 1000);
+    return getSecUntilNextDeparture(queryDate, departureTimes);
+}
+
+function getBrookfieldFerrySec() {
+    return getFerryLeaveSec(new Date(), hobokenToBrookfieldWeekdayDepartureTimes, walkTimeFromAptDoorToFerrySec);
+}
+
+function displayLeaveMinUpdateLoop(divId, leaveSecFunc) {
+    let leaveSec = leaveSecFunc();
+    let leaveMin = Math.floor(leaveSec / 60);
+    // This is probably poor style.
+    document.getElementById(divId).innerHTML = leaveMin;
+    // + .1 is a little hack to make sure that the minute has definitely rolled over by the time we get there.
+    setTimeout(() => displayLeaveMinUpdateLoop(divId, leaveSecFunc), (((leaveSec % 60) + 60) % 60 + .1) * 1000);
+}
+
+displayLeaveMinUpdateLoop('brookfield-ferry-leave-in-min', getBrookfieldFerrySec);
