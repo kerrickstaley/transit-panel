aa1208632f6a04808b68904b259d06f823adb096
Kerrick Staley <kerrick@kerrickstaley.com>
2021-10-31 15:37:49 -0400

    Display WTC PATH times based on schedule, not API
    The API does not have times that are far enough in the future. Hoboken
    is the start of the route anyway, so trains will tend to leave on
    schedule anyway.

--- main.js
+++ main.js
@@ -1,72 +1,5 @@
 "use strict";
 
-var newport_pkwy_station_id = '3199';
-var station_status_url = 'https://gbfs.citibikenyc.com/gbfs/en/station_status.json';
-
-// var request = new XMLHttpRequest();
-// request.open('GET', station_status_url, true);
-// request.onload = function() {
-//     var data = JSON.parse(request.response);
-//     var station_status = data.data.stations.find(
-//         el => el.station_id === newport_pkwy_station_id
-//     );
-//     document.querySelector('body').innerHTML = 'Bikes available: ' + station_status.num_bikes_available;
-// };
-// request.send();
-
-// var request2 = new XMLHttpRequest();
-// request2.open('GET', 'https://path.api.razza.dev/v1/stations/hoboken/realtime');
-// request2.onload = function() {
-//     var data = JSON.parse(request2.response);
-//     console.log(data);
-// };
-// request2.send();
-
-const pathApiUrl = 'https://path.api.razza.dev/v1/stations/hoboken/realtime';
-const walkTimeFromAptDoorToPathSec = 11 * 60;
-
-// Get the number of seconds until all the upcoming PATH departures for a given route and direction.
-//
-// The valid values for route and direction are defined at https://github.com/mrazza/path-data
-//
-// Returns a Promise holding an array of Numbers.
-function getSecsUntilNextPathDepartures(route, direction) {
-    return fetch(pathApiUrl).then(resp => {
-        if (!resp.ok) {
-            throw new Error(`HTTP error fetching PATH API URL: ${response.status}`);
-        }
-        return resp.body.getReader().read();
-    }).then(dataArr => {
-        var data = JSON.parse(new TextDecoder().decode(dataArr.value));
-        var trains = data.upcomingTrains.filter(
-            train => train.route == route && train.direction == direction
-        );
-        return trains.map(train => (Date.parse(train.projectedArrival) - new Date()) / 1000);
-    });
-}
-
-// Returns a promise with the number of seconds until you must leave to catch the next PATH train.
-function getPathLeaveSec(route, direction) {
-    return getSecsUntilNextPathDepartures(route, direction).then(secs => {
-        for (const sec of secs) {
-            const leaveSec = sec - walkTimeFromAptDoorToPathSec;
-            if (leaveSec >= 0) {
-                return leaveSec;
-            }
-        }
-    });
-}
-
-function get33StPathLeaveSec() {
-    return getPathLeaveSec('JSQ_33_HOB', 'TO_NY');
-}
-
-function getWtcPathLeaveSec() {
-    return getPathLeaveSec('HOB_WTC', 'TO_NY');
-}
-
-const walkTimeFromAptDoorToFerrySec = 10 * 60;
-
 // Take a list of departure times, expressed as strings like "8:30 AM", and get the number of seconds
 // from a given Date to those times. Times are interpreted as the next instance of that time, in the
 // local timezone, that is not before the given Date.
@@ -106,13 +39,81 @@ function getSecUntilNextDeparture(date, departureTimes) {
     return Math.min(...getSecUntilDepartures(date, departureTimes));
 }
 
-function getFerryLeaveSec(date, departureTimes, walkTimeSec) {
+function getLeaveSecFromSchedule(date, departureTimes, walkTimeSec) {
     var queryDate = new Date(date * 1 + walkTimeSec * 1000);
     return Promise.resolve(getSecUntilNextDeparture(queryDate, departureTimes));
 }
 
+var newport_pkwy_station_id = '3199';
+var station_status_url = 'https://gbfs.citibikenyc.com/gbfs/en/station_status.json';
+
+// var request = new XMLHttpRequest();
+// request.open('GET', station_status_url, true);
+// request.onload = function() {
+//     var data = JSON.parse(request.response);
+//     var station_status = data.data.stations.find(
+//         el => el.station_id === newport_pkwy_station_id
+//     );
+//     document.querySelector('body').innerHTML = 'Bikes available: ' + station_status.num_bikes_available;
+// };
+// request.send();
+
+// var request2 = new XMLHttpRequest();
+// request2.open('GET', 'https://path.api.razza.dev/v1/stations/hoboken/realtime');
+// request2.onload = function() {
+//     var data = JSON.parse(request2.response);
+//     console.log(data);
+// };
+// request2.send();
+
+const pathApiUrl = 'https://path.api.razza.dev/v1/stations/hoboken/realtime';
+const walkTimeFromAptDoorToPathSec = 11 * 60;
+
+// Get the number of seconds until all the upcoming PATH departures for a given route and direction.
+//
+// The valid values for route and direction are defined at https://github.com/mrazza/path-data
+//
+// Returns a Promise holding an array of Numbers.
+function getSecsUntilNextPathDeparturesFromApi(route, direction) {
+    return fetch(pathApiUrl).then(resp => {
+        if (!resp.ok) {
+            throw new Error(`HTTP error fetching PATH API URL: ${response.status}`);
+        }
+        return resp.body.getReader().read();
+    }).then(dataArr => {
+        var data = JSON.parse(new TextDecoder().decode(dataArr.value));
+        var trains = data.upcomingTrains.filter(
+            train => train.route == route && train.direction == direction
+        );
+        return trains.map(train => (Date.parse(train.projectedArrival) - new Date()) / 1000);
+    });
+}
+
+// Returns a promise with the number of seconds until you must leave to catch the next PATH train,
+// using the API at https://path.api.razza.dev/
+function getPathLeaveSecFromApi(route, direction) {
+    return getSecsUntilNextPathDeparturesFromApi(route, direction).then(secs => {
+        for (const sec of secs) {
+            const leaveSec = sec - walkTimeFromAptDoorToPathSec;
+            if (leaveSec >= 0) {
+                return leaveSec;
+            }
+        }
+    });
+}
+
+function get33StPathLeaveSec() {
+    return getPathLeaveSecFromApi('JSQ_33_HOB', 'TO_NY');
+}
+
+function getWtcPathLeaveSec() {
+    return getLeaveSecFromSchedule(new Date(), hobokenToWtcPathWeekdayDepartureTimes, walkTimeFromAptDoorToPathSec);
+}
+
+const walkTimeFromAptDoorToFerrySec = 10 * 60;
+
 function getBrookfieldFerryLeaveSec() {
-    return getFerryLeaveSec(new Date(), hobokenToBrookfieldWeekdayDepartureTimes, walkTimeFromAptDoorToFerrySec);
+    return getLeaveSecFromSchedule(new Date(), hobokenToBrookfieldFerryWeekdayDepartureTimes, walkTimeFromAptDoorToFerrySec);
 }
 
 // Display remaining minutes before you need to leave in order to catch a given transit option.
