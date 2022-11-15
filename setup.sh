c7871e8cef1f800a7c6344794ecb8c7d58ea18f8
Kerrick Staley <k@kerrickstaley.com>
2022-11-14 22:49:33 -0500

    Add script for setting up dev environment

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -Eeuo pipefail
+cd "$(git rev-parse --show-toplevel)"
+# Add pre-commit hook to re-build bundle.js each commit
+( cd .git/hooks && ln -s ../../git/pre-commit.sh pre-commit )
+# Configure Git to ignore bundle.js changes in output of git diff
+# See https://stackoverflow.com/a/10421385/785404
+git config diff.nodiff.command /bin/true
