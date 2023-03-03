3ec94e7f3946c224ccdf22105c7fd13eb36fe2fb
Kerrick Staley <k@kerrickstaley.com>
2023-03-02 22:23:33 -0500

    Prevent running pre-commit with unstashed changes

--- git/pre-commit.sh
+++ git/pre-commit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -Eeuo pipefail
 cd "$(git rev-parse --show-toplevel)"
+git status --porcelain | egrep '^ M ' > /dev/null && { echo 'unstashed changes'; exit 1; }
 json5 config.json5 > config.json
 git add config.json
 json5 scheduleData.json5 > scheduleData.json
