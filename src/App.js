5a20192e840c70be73d9f35a7b9b28623da2e9d0
Kerrick Staley <k@kerrickstaley.com>
2023-06-02 00:29:07 -0400

    Accept and use get_leave_sec function in Row.js
    The app kinda works after this change!
    
    Things that are probably broken:
    - Hiding a row when it's too far in the future
    - Showing the method in the top right corner
    - It seems that when you refresh the page in dev mode it doesn't
      correctly clear out the old update loop.
    - I'm not regenerating scheduleData.json from scheduleData.json5.
    - Tests are not set up correctly.
    - I'm using snake_case and camelCase inconsistently.
    - I have inconsistent indentation (2 spaces vs 4 spaces).

--- src/App.js
+++ src/App.js
@@ -1,17 +1,70 @@
 import Row from './Row.js';
+import ids from './ids.js';
+import schedule from './schedule.js';
 import train from './images/train.png';
 import ferry from './images/ferry.png';
+import mrazza from './mrazza.js';
+
+const walkTimeFromAptDoorToPathSec = 10 * 60;
+const walkTimeFromAptDoorToFerrySec = 9.5 * 60;
+
+function getLeaveSecGeneric(station, route, walkSec, getDeparturesFuncs) {
+    let promises = getDeparturesFuncs.map(f => f(station, route));
+    return Promise.allSettled(promises).then(allDepartures => {
+        let leaveSecs = allDepartures.flatMap(result => {
+            if (result.status != 'fulfilled') {
+                console.log('Promise broken: ' + JSON.stringify(result));
+                return [];
+            }
+            let leaveSecs = result.value.departures.flatMap(d => {
+                let leaveSec = d - walkSec;
+                return leaveSec >= 0 ? [leaveSec] : [];
+            });
+            return leaveSecs.length >= 1 ? [{leaveSec: leaveSecs[0], method: result.value.method}] : [];
+        });
+        return leaveSecs.length >= 1 ? leaveSecs[0] : null;
+    });
+}
+
+function getLeaveSecPath(station, route, walkSec) {
+    return getLeaveSecGeneric(station, route, walkSec, [mrazza.getDepartures, schedule.getDepartures]);
+}
+
+function getLeaveSecFerry(station, route, walkSec) {
+    return getLeaveSecGeneric(station, route, walkSec, [schedule.getDepartures]);
+}
+
+function getLeaveSec(station, route, walkSec) {
+    let method = {
+        [ids.HOBOKEN]: getLeaveSecPath,
+        [ids.WTC]: getLeaveSecPath,
+        [ids.HOBOKEN_FERRY]: getLeaveSecFerry,
+    }[station];
+    return method(station, route, walkSec);
+}
+
+function getWtcPathLeaveSec() {
+    return getLeaveSec(ids.HOBOKEN, ids.WTC, walkTimeFromAptDoorToPathSec);
+}
+
+function getPathTo33rdLeaveSec() {
+    return getLeaveSec(ids.HOBOKEN, ids._33RD_ST, walkTimeFromAptDoorToPathSec);
+}
+
+function getBrookfieldFerryLeaveSec() {
+    return getLeaveSec(ids.HOBOKEN_FERRY, ids.HOBOKEN_TO_BROOKFIELD_FERRY, walkTimeFromAptDoorToFerrySec);
+}
 
 function App() {
   return (
     <div>
       {/* Original color from PATH website is rgb(70, 156, 35).
         * Lightened 50% using https://pinetools.com/lighten-color */}
-      <Row icon={train} row_title="PATH to WTC" background_color="#99e17c" />
+      <Row row_title="PATH to WTC" get_leave_sec={getWtcPathLeaveSec} icon={train} background_color="#99e17c" />
       {/* Original color from PATH website is rgb(240, 171, 67).
         * Lightened 50% using https://pinetools.com/lighten-color */}
-      <Row icon={train} row_title="PATH to 33rd" background_color="#f7d5a1" />
-      <Row icon={ferry} row_title="Ferry to Brookfield" background_color="#d0e0e3" />
+      <Row row_title="PATH to 33rd" get_leave_sec={getPathTo33rdLeaveSec} icon={train} background_color="#f7d5a1" />
+      <Row row_title="Ferry to Brookfield" get_leave_sec={getBrookfieldFerryLeaveSec} icon={ferry} background_color="#d0e0e3" />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,13 +1,47 @@
+import {useState, useEffect} from 'react';
+import ids from './ids.js';
 import './Row.css';
 
 export default function Row(props) {
-  let {row_title, icon, background_color} = props;
+  const {row_title, icon, background_color, get_leave_sec} = props;
+
+  const [leave_min, set_leave_min] = useState('?');
+
+  let display_leave_min_loop_timeout_id = null;
+  function display_leave_min_loop() {
+    const sec_per_min = 60;  // Can set this to 1 to see the value update every second for testing.
+    get_leave_sec().then(({leaveSec, method}) => {
+        set_leave_min(Math.floor(leaveSec / sec_per_min));
+        let sleep_sec = ((leaveSec % sec_per_min) + sec_per_min) % sec_per_min;
+        if (method == ids.SCHEDULE) {
+            // + .1 is a little hack to make sure that the minute has definitely rolled over by the time we get there.
+            sleep_sec += .1;
+        } else if (ids.isApi(method)) {
+            sleep_sec = Math.min(30, sleep_sec + 5);
+        } else {
+            console.log('unreachable');
+            return;
+        }
+        sleep_sec = Math.max(10, sleep_sec);  // Avoid hitting the API too much
+        display_leave_min_loop_timeout_id = setTimeout(
+          () => display_leave_min_loop(), sleep_sec * 1000);
+    });
+  }
+
+  useEffect(() => {
+    display_leave_min_loop();
+
+    return function cleanup() {
+      clearTimeout(display_leave_min_loop_timeout_id);
+    };
+  }, []);
+
   return <div className="row" style={{backgroundColor: background_color}}>
       <div className="row-title-and-icon">
           <div className="row-title">{row_title}</div>
           <img src={icon} />
       </div>
-      <div className="leave-in-min">?</div>
+      <div className="leave-in-min">{leave_min}</div>
       <div className="spacer"></div>
       <div className="method">?</div>
   </div>;
