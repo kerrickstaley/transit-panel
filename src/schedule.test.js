d231889b81c64d6fb31f418637e326e419212924
Kerrick Staley <k@kerrickstaley.com>
2023-06-04 23:53:01 -0400

    Fix schedule.test.js

--- src/schedule.test.js
+++ src/schedule.test.js
@@ -1,26 +1,38 @@
 import schedule from './schedule.js';
-import ids from './ids.js';
 
-test('getSecUntilDepartures', () => {
-    let now = new Date(1668481604000);
-    expect(schedule.getSecUntilDepartures(now, ["10:07 PM"])).toStrictEqual([16]);
+afterEach(() => {
+    jest.useRealTimers();
 });
 
-test('getDepartures', () => {
-    // 10:06 PM on a Monday
-    // departures after this are 10:10 PM, 10:25 PM, 10:40 PM
-    let now = new Date(1668481560000);
-    let result = schedule.getDeparturesFromTime([[ids.HOBOKEN, ids._33RD_ST]], now, 3);
-    expect(result).toStrictEqual([
-        {
-            station: 'HOBOKEN',
-            route: '_33RD_ST',
-            method: 'SCHEDULE',
-            departures: [
-                4 * 60,
-                19 * 60,
-                34 * 60,
-            ],
-        },
+function getLeaveUpdates(scheduleData, walkSec, nowStr, n = 1) {
+    let ret = null;
+    debugger;
+    jest.useFakeTimers({now: new Date(nowStr)});
+    expect(new Date()).toEqual(new Date(nowStr));
+    // note the () at the end which cancels the pump loop
+    schedule.pumpLeaveUpdates(scheduleData, walkSec, n)(updates => ret = updates)();
+    return ret;
+}
+
+test('getLeaveUpdates basic', () => {
+    let scheduleData = {
+        'sunday': ['10:10 PM', '10:30 PM', '10:50 PM'],
+    };
+    let leaveUpdates = getLeaveUpdates(scheduleData, 10 * 60, '2023-06-04T22:05-04:00', 2);
+    expect(leaveUpdates).toEqual([
+        {leaveTime: new Date('2023-06-04T22:20-04:00'), methodAbbrev: 'SCH'},
+        {leaveTime: new Date('2023-06-04T22:40-04:00'), methodAbbrev: 'SCH'},
+    ]);
+});
+
+test('getLeaveUpdates across day boundary', () => {
+    let scheduleData = {
+        'friday': ['10:00 PM', '11:00 PM'],
+        'weekend': ['5:00 AM'],
+    };
+    let leaveUpdates = getLeaveUpdates(scheduleData, 10 * 60, '2023-06-02T22:30-04:00', 1);
+    expect(leaveUpdates).toEqual([
+        {leaveTime: new Date('2023-06-02T22:50-04:00'), methodAbbrev: 'SCH'},
+        {leaveTime: new Date('2023-06-03T04:50-04:00'), methodAbbrev: 'SCH'},
     ]);
 });
