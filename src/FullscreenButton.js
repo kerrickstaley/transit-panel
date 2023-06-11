f01ec9788f3d9db3739549c6b290f00c62ff5ee6
Kerrick Staley <k@kerrickstaley.com>
2023-06-10 23:47:49 -0400

    Use Wake Lock API to request that screen stays unlocked
    This unfortunately seems to only work when the page is served over
    HTTPS, so I can't test it out in development mode. Also, it only works
    in certain browsers (e.g. Chrome).
    
    Also add an error message that displays if the Wake Lock API is
    unavailable.

--- src/FullscreenButton.js
+++ src/FullscreenButton.js
@@ -2,15 +2,28 @@ import {useState} from 'react';
 
 export default function FullscreenButton() {
     let [visible, setVisible] = useState(true);
+    let [wakeLockErrorVisible, setWakeLockErrorVisible] = useState(false);
 
     function goFullscreen() {
         document.querySelector('body').requestFullscreen().catch(err => {
             alert('Unable to fullscreen: ' + err.message);
         });
+
+        if (navigator.wakeLock == undefined) {
+            setWakeLockErrorVisible(true);
+        } else {
+            navigator.wakeLock.request("screen");
+        }
         setVisible(false);
     }
 
-    return <button onClick={goFullscreen} style={{display: visible ? '' : 'none'}}>
-        Click to go fullscreen
-    </button>;
+    return <>
+        <button onClick={goFullscreen} style={{display: visible ? '' : 'none'}}>
+            Click to go fullscreen and prevent screen from sleeping
+        </button>
+        <div style={{display: wakeLockErrorVisible ? '' : 'none', backgroundColor: '#eeeeee' }}>
+            Unable to prevent screen from sleeping. Try a different browser that supports the Wake
+            Lock API, or check that the page is served over HTTPS.
+        </div>
+    </>;
 }
