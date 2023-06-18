c5428e06bca799ffc6d504dd61cc8f8eb3af6176
Kerrick Staley <k@kerrickstaley.com>
2023-06-18 18:38:08 -0400

    Load config from YAML at specified URL
    Instead of hardcoding the config (i.e. which rows are present), load it
    dynamically from a YAML file at a specified URL. This allows others to
    use the app!
    
    Example URL with my config:
    http://localhost:3000/?config=https://gist.githubusercontent.com/kerrickstaley/515920f7d552bc8027dc57eed4ec76b8/raw/e44fcf5b7b21611ddc041f94b7d90c7265f0149f/transit_panel_config.yaml

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
+import {useState, useEffect, createElement} from 'react';
 import FullscreenButton from './FullscreenButton.js';
-import NyWaterwayFerryRow from './NyWaterwayFerryRow.js';
+import NyWaterwayRow from './NyWaterwayFerryRow.js';
 import NjTransitRailRow from './NjTransitRailRow.js';
 import PathRow from './PathRow.js';
+import YAML from 'yaml';
+
+// TODO would be good if we didn't need this
+const rowComponents = {
+  NjTransitRailRow,
+  NyWaterwayRow,
+  PathRow,
+};
+
+const CONFIG_NOT_IN_URL = 'CONFIG_NOT_IN_URL';
+const CONFIG_LOADING = 'CONFIG_LOADING';
+
+function loadConfig() {
+  const urlParams = new URLSearchParams(window.location.search);
+  let configUrl = urlParams.get('config');
+  if (configUrl === null) {
+    return Promise.resolve(CONFIG_NOT_IN_URL);
+  }
+  return fetch(configUrl).then(resp => resp.text()).then(text => YAML.parse(text));
+}
 
 function App() {
-  return (
-    <div>
-      <PathRow origin="hoboken" destination="worldTradeCenter" rowTitle="PATH to WTC" walkSec={600} />
-      <PathRow origin="hoboken" destination="thirtyThirdStreet" rowTitle="PATH to 33rd" walkSec={600} />
-      <NyWaterwayFerryRow origin="hoboken" destination="brookfield" walkSec={600} />
-      <NjTransitRailRow origin="hoboken" routeName="bntnm" walkSec={660} rowTitle="BNTNM to Newark" />
-      <FullscreenButton />
-    </div>
-  );
+  let [config, setConfig] = useState(CONFIG_LOADING);
+
+  useEffect(() => {
+    loadConfig().then(yaml => setConfig(yaml));
+  }, []);
+
+  if (config === CONFIG_LOADING) {
+    return <div>Loading config...</div>;
+  } else if (config === CONFIG_NOT_IN_URL) {
+    return <div>
+      config param not specified in URL! Please put ?config=&lt;your config YAML URL&gt; in the URL.
+    </div>;
+  }
+
+  let rows = [];
+  for (let row of config.rows) {
+    let rowComponentName = row.type.charAt(0).toUpperCase() + row.type.substring(1) + 'Row';
+    let rowComponent = rowComponents[rowComponentName];
+    let props = Object.assign({}, row);
+    delete props['type'];
+    // TODO fix
+    props['walkSec'] = props['walkMinutes'] * 60;
+    delete props['walkMinutes'];
+    props['rowTitle'] = props['title'];
+    delete props['title'];
+    rows.push(createElement(rowComponent, props));
+  }
+  return createElement('div', {}, ...rows, <FullscreenButton />);
 }
 
 export default App;
