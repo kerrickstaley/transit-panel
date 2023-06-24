aaf74408b74b593f5d78d868912906af625548af
Kerrick Staley <k@kerrickstaley.com>
2023-06-24 14:31:26 -0700

    Fix tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders loading config text', () => {
+test('renders config param not specified text', () => {
   render(<App />);
-  const loadingConfigDiv = screen.getByText(/loading config/i);
-  expect(loadingConfigDiv).toBeInTheDocument();
+  const configNotSpecifiedDiv = screen.getByText(/config.*not specified/i);
+  expect(configNotSpecifiedDiv).toBeInTheDocument();
 });
